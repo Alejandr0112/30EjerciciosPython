0e8ad91527862a32f8ba05e2e34e12bc2c7e05f2
Alejandr0112 <erickalejandro0112@gmail.com>
2025-10-18 08:34:05 -0600

    VocalConsonante

---
+++ If - Else/VocalConsonante.py
@@ -0,0 +1,8 @@
+letra = input("Ingresa una letra: ").lower()
+
+if len(letra) != 1 or not letra.isalpha():
+    print("Por favor, ingresa solo una letra.")
+elif letra in "aeiou":
+    print("La letra es una vocal.")
+else:
+    print("La letra es una consonante.")
