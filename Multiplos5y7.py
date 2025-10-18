1056754bc758706c8b2b72d55d50c96834b556f0
Alejandr0112 <erickalejandro0112@gmail.com>
2025-10-18 08:40:42 -0600

    MultiplosDe5y7

---
+++ Multiplos5y7.py
@@ -0,0 +1,6 @@
+numero = int(input("Ingresa un número: "))
+
+if numero % 5 == 0 and numero % 7 == 0:
+    print("El número es múltiplo de 5 y de 7.")
+else:
+    print("El número no es múltiplo de 5 y de 7.")
